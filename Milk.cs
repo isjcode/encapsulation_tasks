da12ec0a5a718b364cb24be062caa4c59e581cfc
isjcode <98400721+isjcode@users.noreply.github.com>
2022-02-13 22:46:46 -0800

    Add files via upload

---
+++ Milk.cs
@@ -0,0 +1,14 @@
+﻿namespace USER_TASK
+{
+    class Milk : Product
+    {
+        public int Volume;
+        public int FatRate;
+
+        public Milk(string a_name, int a_price, int a_count, int a_total_income, int volume, int fat_rate) : base(a_name, a_price, a_count, a_total_income)
+        {
+            Volume = volume;   
+            FatRate = fat_rate;
+        }
+    }
+}
