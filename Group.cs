e9f574e48c15c6e33b076947bef9d26ced79ef70
isjcode <98400721+isjcode@users.noreply.github.com>
2022-02-13 23:36:40 -0800

    Add files via upload

---
+++ Group.cs
@@ -0,0 +1,43 @@
+﻿namespace USER_TASK
+{
+    class Group
+    {
+        public int No;
+        public int Limit;
+        private Student[] Students;
+
+        public Group(int a_no, int a_limit, Student [] arr)
+        {
+            No = a_no;
+            Limit = a_limit;
+            Students = arr;
+
+        }
+
+        public void AddStudent(Student student)
+        {
+            if (Limit > Students.Length)
+            {
+;
+                Array.Resize(ref Students, Students.Length + 1);
+                Students[Students.Length-1] = student;
+  
+            }
+            else
+            {
+             
+                Console.WriteLine("Qrup Dolub.");
+                return;
+            }
+        }
+
+        public void GetStudents()
+        {
+            foreach (Student student in Students)
+            {
+                Console.WriteLine(student.Name + " " + student.Surname);
+
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,31 @@
+﻿namespace USER_TASK
+{
+
+    class aa
+    {
+        static void Main(string[] args)
+        {
+            Student stu1 = new Student("sda", "DASDAS");
+            Student stu2 = new Student("sda", "DASDAS");
+
+            Student[] arr = { };
+
+            Group group = new Group(33, 2, arr);
+
+            group.AddStudent(stu1);
+            group.AddStudent(stu2);
+            group.GetStudents();
+        }
+    }
+    class Student
+    {
+        public string Name;
+        public string Surname;
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
