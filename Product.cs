2f62e287a3d7746ec534deb26c81a0ba6234fc42
isjcode <98400721+isjcode@users.noreply.github.com>
2022-02-13 22:18:29 -0800

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,31 @@
+﻿namespace USER_TASK
+{
+    class Product
+    {
+        public string product_name;
+        public int product_price;
+        private int count;
+        public int total_income;
+
+        public Product (string a_name, int a_price, int a_count, int a_total_income)
+        {
+            product_name = a_name;
+            product_price = a_price;
+            count = a_count;
+            total_income = a_total_income;
+        }
+
+        public void Sell()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Product bitib.");
+                return;
+            }
+
+            count--;
+            total_income += product_price;
+            Console.WriteLine("Satildi.");
+        }
+    }
+}
